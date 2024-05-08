90ff2cd5a7e6335739986f5520d37c3ce81d8d7a
S.Matuzov <ven0m2031@mail.ru>
2024-05-08 14:28:56 +0400

    Фикс красных, балки, начало танка

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,6 +1,6 @@
 extends Button
 @export var diraction_array : Node2D
-#@export var attack_prefab : Node2D
+@export var attack_prefab : Node2D
 
 
 
@@ -12,7 +12,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	global_position = global_position.move_toward(diraction_array.current_position.position, 3.5)
+	if attack_prefab.is_attack != true:
+		global_position = global_position.move_toward(diraction_array.current_position.position, 3.5)
 	#if attack_prefab != null:			
 	#	if not attack_prefab.is_attack:	
 	#		pass
@@ -21,6 +22,3 @@ func _on_button_down():
 	queue_free()
 
 
-func _on_pressed():
-	print("pressed")
-	pass # Replace with function body.
--- Scripts/RedTrilobiteSpawnPoint.gd
+++ Scripts/RedTrilobiteSpawnPoint.gd
@@ -6,5 +6,5 @@ var numbers_of_enemies = 5
 func _on_timer_timeout():
 	if numbers_of_enemies > 0:
 		numbers_of_enemies -= 1
-		var enemy = load("res://button.tscn").instantiate()
+		var enemy = load("res://Prefabs/enemies/redBug.tscn").instantiate()
 		add_child(enemy)
