278db903af9d1e3a4ba3d3f0b5bf4f0d75c4e629
S.Matuzov <ven0m2031@mail.ru>
2024-05-15 16:44:55 +0400

    добавлен контроль таймера выстрелов врагов

--- Scripts/RedTrilobiteSpawnPoint.gd
+++ Scripts/RedTrilobiteSpawnPoint.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-
 var numbers_of_enemies = 25
+var limit_of_enemies = 5
 var arr : Array
 
 func _on_timer_timeout():
-	if numbers_of_enemies > 0 and arr.size() <= 4:
+	if numbers_of_enemies > 0 and arr.size() <= (limit_of_enemies-1):
 		numbers_of_enemies -= 1
 		var enemy = load("res://Prefabs/enemies/redBug.tscn").instantiate()
 		add_child(enemy)
--- red_bug_attack.gd
+++ red_bug_attack.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @export var enemy_sprite : CanvasItem
+@export var time_to_shoot : int
+@export var is_tank : bool
+@export_global_file ("res://Assets/Flame.png") var red_attack
 
 var is_attack : bool
 var arr : Array
@@ -8,21 +11,27 @@ var original_color : Color
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
+	print(red_attack)		
 	original_color = enemy_sprite.modulate
 	is_attack = false
-
+	%Timer.start(time_to_shoot)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if enemy_sprite != null:
 		if %Timer.get_time_left() < 1.1:		
-			enemy_sprite.modulate = Color(1, 0, 0, 1)
-	
+			enemy_sprite.modulate = Color(1, 0, 0, 1)	
 
 
 func _on_timer_timeout():
-	var flame = load("res://Prefabs/flame.tscn").instantiate()
+	var flame 
+	if is_tank:
+		flame = load("res://Prefabs/bomb.tscn").instantiate()
+	else:
+		flame = load("res://Prefabs/flame.tscn").instantiate()
 	flame.position = position
+	enemy_sprite.modulate = original_color
 	add_child(flame)
 	arr.append(flame)
 	is_attack = true
@@ -30,4 +39,4 @@ func _on_timer_timeout():
 	is_attack = false
 	arr[0].queue_free()
 	arr=[]
-	enemy_sprite.modulate = original_color
+	
