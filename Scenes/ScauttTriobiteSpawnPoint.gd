cff4c0d7c6ce62659dda1b5d1e3025de8d44c009
S.Matuzov <ven0m2031@mail.ru>
2024-05-22 16:48:01 +0400

    added scout

---
+++ Scenes/ScauttTriobiteSpawnPoint.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var numbers_of_enemies = 17
+var arr : Array
+
+func _on_timer_timeout():
+	if numbers_of_enemies > 0 and arr.size() < 1:
+		numbers_of_enemies -= 1
+		var enemy = load("res://Prefabs/enemies/scoutt.tscn").instantiate()
+		add_child(enemy)
+		arr.append(enemy)
+
+func clear_arr_element():
+	arr.resize(arr.size() - 1)
--- Scripts/enemies/EnemyCore.gd
+++ Scripts/enemies/EnemyCore.gd
@@ -2,11 +2,12 @@ extends Button
 @export var diraction_array : Node2D
 @export var attack_prefab : Node2D
 @export var enemy_health : int
+@export var enemy_speed: float
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if attack_prefab.is_attack != true:
-		global_position = global_position.move_toward(diraction_array.current_position.position, 3.5)
+		global_position = global_position.move_toward(diraction_array.current_position.position, enemy_speed)
 	if enemy_health == 0:
 			var par = get_parent()
 			par.clear_arr_element()
