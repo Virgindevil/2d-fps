14277a816d8e74217512d54a434f9e4458d40a8f
S.Matuzov <ven0m2031@mail.ru>
2024-05-24 16:38:27 +0400

    reload fix

--- Prefabs/Bonuses/automata.gd
+++ Prefabs/Bonuses/automata.gd
@@ -5,8 +5,10 @@ func _process(delta):
 
 func _on_button_down():
 	GlobalVars.ammo = 30
-	GlobalVars.is_automata_pickup = true
-	queue_free()
+	GlobalVars.is_automata_pickup = true	
+	visible = false
+	disabled = true
 
 func _on_timer_timeout():
+	GlobalVars.is_automata_active = false
 	queue_free()
